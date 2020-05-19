6924752de133b655639efaa256fb55c4af23a1c3
Your Name <the.jeremy.cobb@gmail.com>
2020-05-19 10:08:27 -0500

    added .env file & notes and modified .gitignore

---
+++ env.py
@@ -0,0 +1,12 @@
+
+# get url function 
+
+host = "157.230.209.171"
+user = "curie_944"
+password = "A6LUTZA9MPbUSy89niNy0Fd9qyI2tF8h"
+
+def get_url(database):
+    host = "157.230.209.171"
+    user = "curie_944"
+    password = "A6LUTZA9MPbUSy89niNy0Fd9qyI2tF8h"
+    return f'mysql+pymysql://{user}:{password}@{host}/{database}'
